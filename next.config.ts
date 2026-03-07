21180c0d726c113be721fdca28b5cedd56dfe404
Chaen <cachagn@sketecthailand.com>
2026-03-07 17:00:26 +0700

    chore: Remove hardcoded fallback value for RESEND_API_KEY.

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || 'your-secret-key-change-in-production',
     NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://suankhundaeng.appviza.com',
     JWT_SECRET: process.env.JWT_SECRET || 'fallback-for-development-jwt-secret',
-    RESEND_API_KEY: process.env.RESEND_API_KEY || 're_YLJbkUnu_Jg7ukozLjeyB3rVESyftnuAA'
+    RESEND_API_KEY: process.env.RESEND_API_KEY
   },
   images: {
     remotePatterns: [
