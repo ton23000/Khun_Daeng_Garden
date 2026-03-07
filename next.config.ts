795b1e7a64c5383447bcfd9f4190432a0a967de7
Chaen <cachagn@sketecthailand.com>
2026-03-07 17:31:15 +0700

    feat: add NEXT_PUBLIC_APP_URL to environment variables.

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,8 @@ const nextConfig: NextConfig = {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || 'your-secret-key-change-in-production',
     NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://suankhundaeng.appviza.com',
     JWT_SECRET: process.env.JWT_SECRET || 'fallback-for-development-jwt-secret',
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || 'https://khundaenggarden.vercel.app'
   },
   images: {
     remotePatterns: [
