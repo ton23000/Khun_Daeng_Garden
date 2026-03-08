51e6404c37ee765c1f6fd5e68f32231422622636
Chaen <cachagn@sketecthailand.com>
2026-03-08 23:44:33 +0700

    refactor: Move Prisma seed configuration from `prisma.config.ts` to `package.json`.

--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-// Prisma configuration
-// See https://pris.ly/d/prisma-config for more options
-
-const config = {
-  seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
-};
-
-export default config;
